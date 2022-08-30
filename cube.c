2bd9583af8c0a2fc70b937bf960a823a853a44e9
meghasoni <110007477+1610megha@users.noreply.github.com>
2022-08-30 08:25:39 +0530

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float Amt,Ans;
+	printf("Enter the no for cube : \n");
+	scanf("\n %f", &Amt);
+	Ans=Amt*Amt*Amt;
+	printf("\nCube of the given no is: %f", Ans);
+}
