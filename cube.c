027a606a69e5bdeff90f193275cd0f98add834f7
meghasoni <110007477+1610megha@users.noreply.github.com>
2022-11-15 16:51:05 +0530

    Delete cube.c

--- cube.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	float Amt,Ans;
-	printf("Enter the no for cube : \n");
-	scanf("\n %f", &Amt);
-	Ans=Amt*Amt*Amt;
-	printf("\nCube of the given no is: %f", Ans);
-}
